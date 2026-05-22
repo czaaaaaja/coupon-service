d297589d24aa9a5c86c6626410d5d33db6b34c25
Piotr Czajkowski <piotr@MacBook-Air-Piotr-2.local>
2026-05-22 19:28:07 +0200

    init commit

---
+++ src/test/java/com/recruitment/empik/coupon_service/CouponServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recruitment.empik.coupon_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
